618aafdbb13405649f52af459ee6d3ed7c9a51f3
Daniel Wallman <danielwallman@MacBook-Pro-som-tillhor-Daniel.local>
2022-01-15 18:53:13 +0100

    first commit

---
+++ src/main/java/com/wallman/helloservice/HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wallman.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallman/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallman.helloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
