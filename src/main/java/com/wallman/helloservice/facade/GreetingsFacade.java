aab941856777ec5e3ce195aebe62c34de2d799aa
Daniel Wallman <daniel.wallman94@gmail.com>
2022-01-16 21:36:45 +0100

    authz

--- src/main/java/com/wallman/helloservice/facade/GreetingsFacade.java
+++ src/main/java/com/wallman/helloservice/facade/GreetingsFacade.java
@@ -1,6 +1,7 @@
 package com.wallman.helloservice.facade;
 
 import com.wallman.helloservice.domain.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,9 +10,13 @@ import reactor.core.publisher.Mono;
 public class GreetingsFacade {
 
     private static final String GREETING_SERVICE_URL = "https://greeting-message-m6cqmyea3a-lz.a.run.app/message";
+    private final WebClient client;
+
+    public GreetingsFacade(@Autowired WebClient client) {
+        this.client = client;
+    }
 
     public Mono<Greeting> get() {
-        WebClient client = WebClient.create();
         return client.get()
                 .uri(GREETING_SERVICE_URL)
                 .retrieve()
